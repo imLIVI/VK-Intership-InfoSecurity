3cd988b9610323a8d61da8a5b78eb40fb45ed530
imLIVI <lesly120898@gmail.com>
2023-05-06 22:30:33 +0300

    Add min protection from XSS

--- src/main/java/vk/security/vkintershipinfosecurity/service/AuthorizationService.java
+++ src/main/java/vk/security/vkintershipinfosecurity/service/AuthorizationService.java
@@ -5,6 +5,8 @@ import vk.security.vkintershipinfosecurity.exceptions.InvalidCredentials;
 import vk.security.vkintershipinfosecurity.exceptions.UnauthorizedUser;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class AuthorizationService {
@@ -22,11 +24,19 @@ public class AuthorizationService {
         if (isEmpty(user) || isEmpty(password)) {
             throw new InvalidCredentials("User name or password is empty");
         }
+        user = protectionXSS(user);
+        password = protectionXSS(password);
         List<Authorities> userAuthorities = userRepository.getUserAuthorities(user, password);
         if (isEmpty(userAuthorities)) {
             throw new UnauthorizedUser("Unknown user " + user);
         }
         return userAuthorities;
+    }
+
+    public String protectionXSS(String str) {
+        Pattern pattern = Pattern.compile("[&<>\"']");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.replaceAll("");
     }
 
     private boolean isEmpty(String str) {
